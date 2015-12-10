782af28aacc56c57405bcf07d48ba45c3b4c80f8
Ikey Doherty <ikey@solus-project.com>
2015-12-10 09:33:46 +0000

    For now completely disable delta generation
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- binman.py
+++ binman.py
@@ -440,10 +440,10 @@ class BinMan(pyinotify.ProcessEvent):
                         continue
                     os.chdir(deltadir)
                     pkgs = None
-                    try:
-                        pkgs = pisi.operations.delta.create_delta_package(i.filename, top.filename)
-                    except Exception, e:
-                        print "Generic delta issue: %s" % e
+                    #try:
+                    #    pkgs = pisi.operations.delta.create_delta_package(i.filename, top.filename)
+                    #except Exception, e:
+                    #    print "Generic delta issue: %s" % e
                     if not pkgs or len(pkgs) == 0:
                         print "No delta possible for %s-%s" % (i.pkg.package.name, i.pkg.package.version)
                     else:
