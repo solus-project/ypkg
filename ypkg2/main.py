e8b7b628de6a4c9aac026b39064076db9afccbcf
Ikey Doherty <ikey@solus-project.com>
2016-04-05 22:46:51 +0100

    Relicense project under GPL3 - borrowed autospec bits and mixed license is hard
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- ypkg2/main.py
+++ ypkg2/main.py
@@ -5,9 +5,9 @@
 #
 #  Copyright 2015-2016 Ikey Doherty <ikey@solus-project.com>
 #
-#  This program is free software; you can redistribute it and/or modify
+#  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
+#  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
 
