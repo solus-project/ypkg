2fa61e88725a3cfb3e6a134526aa4eaf9a0010b7
Ikey Doherty <ikey@solus-project.com>
2016-04-07 00:57:42 +0100

    Split off main execution into a "ypkg-build" script
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-pep8 ypkg2/*.py || exit 1
+pep8 ypkg-build ypkg2/*.py || exit 1
 #for item in examples/*.yml ; do
 #    python -m ypkg2.main $item || exit 1
 #done
 
-fakeroot python -m ypkg2.main examples/nano.yml || exit 1
+fakeroot ./ypkg-build examples/nano.yml || exit 1
