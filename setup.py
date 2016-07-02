80bb3e1138f321d8042c92d48a25323ec5a98dbd
Ikey Doherty <ikey@solus-project.com>
2016-07-02 16:55:14 +0100

    Release v10
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "ypkg2",
-    version = "9.0.0",
+    version = "10.0.0",
     author = "Ikey Doherty",
     author_email = "ikey@solus-project.com",
     description = ("Solus YPKG build Tool"),
