c02a9400c5eb576e6e3abf0edf32b5fafbe55d17
Ikey Doherty <ikey@solus-project.com>
2016-12-06 05:22:31 +0000

    man: Begin the package.yml.5 documentation
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- gen_docs.sh
+++ gen_docs.sh
@@ -2,7 +2,7 @@
 
 # Credit to swupd developers: https://github.com/clearlinux/swupd-client
 
-MANPAGES="man/ypkg.1 man/ypkg-install-deps.1 man/ypkg-build.1"
+MANPAGES="man/ypkg.1 man/ypkg-install-deps.1 man/ypkg-build.1 man/package.yml.5"
 
 for MANPAGE in ${MANPAGES}; do \
     ronn --roff < ${MANPAGE}.md > ${MANPAGE}; \
