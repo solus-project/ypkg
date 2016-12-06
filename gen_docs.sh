9a0984caf0ca4de93eea9686bde29a6d2e5bf980
Ikey Doherty <ikey@solus-project.com>
2016-12-06 05:03:46 +0000

    man: Build manpages for ypkg-install-deps.1
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- gen_docs.sh
+++ gen_docs.sh
@@ -2,7 +2,7 @@
 
 # Credit to swupd developers: https://github.com/clearlinux/swupd-client
 
-MANPAGES="man/ypkg.1"
+MANPAGES="man/ypkg.1 man/ypkg-install-deps.1"
 
 for MANPAGE in ${MANPAGES}; do \
     ronn --roff < ${MANPAGE}.md > ${MANPAGE}; \
