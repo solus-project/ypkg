e0dc77f145a54bb847d6f3f864468bca17a1dba7
Ikey Doherty <ikey@solus-project.com>
2017-02-24 21:45:05 +0000

    Legit finish off the copyright data fixes
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- ypkg2/main.py
+++ ypkg2/main.py
@@ -37,7 +37,7 @@ from configobj import ConfigObj
 
 def show_version():
     print("Ypkg - Package Build Tool")
-    print("\nCopyright (C) 2015-2016 Ikey Doherty\n")
+    print("\nCopyright (C) 2015-2017 Ikey Doherty\n")
     print("This program is free software; you are free to redistribute it\n"
           "and/or modify it under the terms of the GNU General Public License"
           "\nas published by the Free Software foundation; either version 3 of"
